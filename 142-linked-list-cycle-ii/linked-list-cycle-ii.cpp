e3000ee0ae6899a55f34638e9f251d3319f49f1f
praks-shetha <23bda046@kprcas.ac.in>
2025-05-17 22:01:51 +0530

    Time: 10 ms (46.23%) | Memory: 11.4 MB (53.28%) - LeetSync

--- 142-linked-list-cycle-ii/linked-list-cycle-ii.cpp
+++ 142-linked-list-cycle-ii/linked-list-cycle-ii.cpp
@@ -9,13 +9,34 @@
 class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {
-        unordered_set<ListNode*>s;
+        /*unordered_set<ListNode*>s;
         while(head)
         {
             if(s.count(head)==1)return head;
             s.insert(head);
             head=head->next;
         }
-        return NULL;
+        return NULL;*/
+        ListNode* slow=head;
+        ListNode* fast=head;
+        while(fast)
+        {
+            fast=fast->next;
+            if(fast)
+            {
+                fast=fast->next;
+                slow=slow->next;
+            }
+            if(fast==slow)break;
+
+        }
+        if(fast==nullptr)return nullptr;
+        slow=head;
+        while(fast!=slow)
+        {
+            fast=fast->next;
+            slow=slow->next;
+        }
+        return slow;
     }
 };
