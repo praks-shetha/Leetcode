a00d884471ef6e3a081ef2389a9388fda7532609
praks-shetha <23bda046@kprcas.ac.in>
2025-05-21 17:53:15 +0530

    Time: 4 ms (34.53%) | Memory: 15.2 MB (37.06%) - LeetSync

--- 1741-sort-array-by-increasing-frequency/sort-array-by-increasing-frequency.cpp
+++ 1741-sort-array-by-increasing-frequency/sort-array-by-increasing-frequency.cpp
@@ -1,6 +1,17 @@
 class Solution {
 public:
     vector<int> frequencySort(vector<int>& nums) {
+       /* unordered_map<int,int>m;
+        for(auto i:nums)
+        {
+            m[i]++;
+        }
+        sort(nums.begin(),nums.end(),[&](int a,int b){
+            return m[a]==m[b]?a>b:m[a]<m[b];
+        });
+        return nums;*/
+
+
         unordered_map<int,int>m;
         for(auto i:nums)
         {
@@ -10,5 +21,6 @@ public:
             return m[a]==m[b]?a>b:m[a]<m[b];
         });
         return nums;
+
     }
 };
