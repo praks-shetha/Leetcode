46d50ecfdab70ce033e1462ec1987d7e393bf7c5
praks-shetha <23bda046@kprcas.ac.in>
2025-05-25 20:33:35 +0530

    Time: 7 ms (18.35%) | Memory: 12.5 MB (83.84%) - LeetSync

--- 1287-distance-between-bus-stops/distance-between-bus-stops.cpp
+++ 1287-distance-between-bus-stops/distance-between-bus-stops.cpp
@@ -2,17 +2,16 @@ class Solution {
 public:
     int distanceBetweenBusStops(vector<int>& distance, int start, int destination) {
         if(start>destination)swap(start,destination);
-        int total=0;
-        int anticlock=0;
-        int clock=0;
+        int total=0;int anticlock=0;int clock=0;
+       
         for(int i=0;i<distance.size();i++)
         {
-            total+=distance[i];
-            if(i>=start && i<destination)
+            total+=distance[i];//total dis
+            if(i>=start && i<destination)//to find clock 
             {
                 clock+=distance[i];
             }
-            anticlock=total-clock;
+            anticlock=total-clock; //if total is 10 then clock is 6 the anticlock will be that
         }
         return min(clock,anticlock);
     }
